a8da24081eb9a36dc771e920c2a7a166b4ec057a
alias313 <eliesgarciaalvira@gmail.com>
2024-03-12 15:34:35 +0100

    Deleted PRACTICA_1 directory

--- PRACTICA_1/src/practica2/P0CZ/Monitor/TestSumCZ.java
+++
@@ -1,8 +0,0 @@
-package practica2.P0CZ.Monitor;
-
-public class TestSumCZ {
-
-    public static void main(String[] args) throws InterruptedException {
-        throw new RuntimeException("//Completar...");
-    }
-}
--- PRACTICA_1/src/util/SimNet.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-public interface SimNet {
-
-  void       send(TCPSegment seg);
-  TCPSegment receive();
-  int        getMTU();  // MTU: Maximum Transmission Unit (Link Layer)
-  
-}
