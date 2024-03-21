0b99f7dad80d6bae4d5f5e2a06291d182d3b08ab
alias313 <eliesgarciaalvira@gmail.com>
2024-03-21 17:24:26 +0100

    Added Broadcast package

---
+++ Monitors/ExercicisMonitors/src/main/java/Broadcast/BufferBroadcastUn.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Broadcast;
+
+/**
+ *
+ * @author ele
+ */
+public class BufferBroadcastUn {
+    protected Object espai;
+    protected boolean[] disponible;
+    // disponible[i] indica que el i-essim consumidor
+    // no ha consumit l'objected actual en espai
+    
+}
