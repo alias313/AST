f338470df6a9728663ade954094927a1d923903b
alias313 <eliesgarciaalvira@gmail.com>
2024-04-03 09:23:41 +0200

    CompteEstalvis part B

--- Monitors/ExercicisMonitors/src/main/java/CompteEstalvis/CompteEstalvis.java
+++ Monitors/ExercicisMonitors/src/main/java/CompteEstalvis/CompteEstalvis.java
@@ -4,34 +4,47 @@
  */
 package CompteEstalvis;
 
+import java.util.ArrayList;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 
 /**
  *
- * @author ele
+ * PART B: LES EXTRACCIONS S'HAN DE FER EN ORDRE D'ARRIBADA
  */
 public class CompteEstalvis {
-    protected int balanç; // Balanç en centims
+    protected int balanc; // Balanç en centims
     protected Lock mon;
     protected Condition potExtreure;
+    protected ArrayList<Condition> treure;
+    protected ArrayList<Integer> quantitatATreure;
     
-    // que incrementa el balanç del compte en quantitat
+    // que incrementa el balanc del compte en quantitat
     public void depositar(int quantitat) {
         mon.lock();
-        balanç = balanç + quantitat;
-        potExtreure.signalAll();
+        balanc = balanc + quantitat;
+        if (quantitatATreure.isEmpty() && quantitatATreure.get(0) < balanc) {
+            treure.get(0).signal();
+        }
         mon.unlock();
     }
     
-    // que extreu quantitat del balanç del compte.
-    // Si el balanç quedes negatiu llavors s'ha d'esperar a poder extreure
+    // que extreu quantitat del balanc del compte.
+    // Si el balanc quedes negatiu llavors s'ha d'esperar a poder extreure
     public void extreure(int quantitat) throws InterruptedException {
         mon.lock();
-        while (balanç < quantitat) {
-            potExtreure.await();
+        if (!treure.isEmpty() || balanc < quantitat) {
+            Condition cond = mon.newCondition();
+            treure.add(cond);
+            quantitatATreure.add(quantitat);
+            cond.await();
+            treure.remove(0);
+            quantitatATreure.remove(0);
         }
-        balanç = balanç - quantitat;
+        balanc = balanc - quantitat;
+        if (!treure.isEmpty() && quantitatATreure.get(0) < balanc) {
+            treure.get(0).signal();
+        }
         mon.unlock();
     }
 }
