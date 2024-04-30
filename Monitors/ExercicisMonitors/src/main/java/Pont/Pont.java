8824954fe4afb9e27b433d63a487056307152c70
Elies García Alvira <eliesgarciaalvira@gmail.com>
2024-04-30 21:33:39 +0200

    Pont llevaids completat (amb comptadors)

--- Monitors/ExercicisMonitors/src/main/java/Pont/Pont.java
+++ Monitors/ExercicisMonitors/src/main/java/Pont/Pont.java
@@ -6,11 +6,14 @@ import java.util.concurrent.locks.Condition;
 
 public class Pont {
     protected Lock mon;
-    protected Condition cotxePassa, vaixellPassa;
+    protected Condition cotxePassa, vaixellPassa, deixaSortir;
     protected char estatActual; // 'v' es obert/aixecat (passen vaixells), 'c' es tancat/abaixat (passen cotxes)
+    protected int cotxesEntrat, cotxesSortit, vaixellsEntrat, vaixellsSortit;
+    protected boolean canviant;
 
     public Pont(char estatInicial) {
         this.mon = new ReentrantLock();
+        this.deixaSortir = mon.newCondition();
         this.cotxePassa = mon.newCondition();
         this.vaixellPassa = mon.newCondition();
         this.estatActual = estatInicial;
@@ -18,7 +21,7 @@ public class Pont {
     public void entrar(char tipus) {
         try {
             mon.lock();
-            if (Character.compare(tipus, estatActual) != 0) {
+            while (Character.compare(tipus, estatActual) != 0) {
                 if (tipus == 'c') {
                     cotxePassa.await();
                 }
@@ -31,9 +34,11 @@ public class Pont {
                 }
             }
             if (tipus == 'c') {
+                cotxesEntrat++;
                 System.out.println("COTXE ENTRA");
             }
             else if (tipus == 'v') {
+                vaixellsEntrat++;
                 System.out.println("VAIXELL ENTRA");
             }
             else {
@@ -51,9 +56,17 @@ public class Pont {
         try {
             mon.lock();
             if (tipus == 'c') {
+                cotxesSortit++;
+                if (canviant && cotxesEntrat == cotxesSortit) {
+                    deixaSortir.signal();
+                }
                 System.out.println("COTXE SURT");
             }
             else if (tipus == 'v') {
+                vaixellsSortit++;
+                if (canviant && vaixellsEntrat == vaixellsSortit) {
+                    deixaSortir.signal();
+                }
                 System.out.println("VAIXELL SURT");
             }
             else {
@@ -71,11 +84,23 @@ public class Pont {
         try {
             mon.lock();
             if (estatActual == 'c') {
+                while (cotxesEntrat != cotxesSortit) {
+                    System.out.println("COTXES: " + cotxesEntrat + " " + cotxesSortit);
+                    canviant = true;
+                    deixaSortir.await();
+                }
+                canviant = false;
                 estatActual = 'v';
                 vaixellPassa.signalAll();
                 System.out.println("PONT AIXECAT");
             }
             else if (estatActual == 'v') {
+                while (vaixellsEntrat != vaixellsSortit) {
+                    System.out.println("VAIXELLS: " + vaixellsEntrat + " " + vaixellsSortit);
+                    canviant = true;
+                    deixaSortir.await();
+                }
+                canviant = false;
                 estatActual = 'c';
                 cotxePassa.signalAll();
                 System.out.println("PONT ABAIXAT");
--- Monitors/ExercicisMonitors/src/main/java/Pont/PontTest.java
+++ Monitors/ExercicisMonitors/src/main/java/Pont/PontTest.java
@@ -1,11 +1,13 @@
 package Pont;
 
+import java.util.Random;
+
 public class PontTest extends Thread {
     public static void main(String[] args) {
 
         Pont monitor = new Pont('c');
         Thread cotxe, vaixell;
-        int nombreCotxes = 500;
+        int nombreCotxes = 200;
 
         for (int i = 0; i < nombreCotxes; i++) {
             if (i % 10 == 0) {
@@ -15,16 +17,37 @@ public class PontTest extends Thread {
             if (i % 5 == 0) {
                 vaixell = new Thread(() -> {
                     monitor.entrar('v');
+                    try {
+                        Random random = new Random();
+                        Thread.sleep(random.nextInt(100));
+                    } catch (Exception ex) {
+                        System.out.println(ex);
+                    }
+            
                     monitor.sortir('v');
                 });
                 vaixell.start();
             }
             cotxe = new Thread(() -> { 
-                    monitor.entrar('c'); 
-                    monitor.sortir('c');
+                monitor.entrar('c');
+                try {
+                    Random random = new Random();
+                    Thread.sleep(random.nextInt(100));
+                } catch (Exception ex) {
+                    System.out.println(ex);
+                }
+                monitor.sortir('c');
             });
             cotxe.start();
+            System.out.println("FINISHED ITERATION NUMBER ----------- " + i);
         }
         monitor.canviar();
+        System.out.println("SLEEP");
+        try {
+            Thread.sleep(1000);
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        System.out.println("AWAKEN");
     }
 }
