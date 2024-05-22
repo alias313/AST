60888509591748f8ea8906d64e04d5b4640d4e33
Elies García Alvira <eliesgarciaalvira@gmail.com>
2024-05-22 02:12:24 +0200

    Added visual std out for N/S cars

--- exercicisTransport/OneLaneBridgeDistribuit/src/AplicacioEcho/Client.java
+++ exercicisTransport/OneLaneBridgeDistribuit/src/AplicacioEcho/Client.java
@@ -66,6 +66,16 @@ class ClientCar implements Runnable{
 }
 
 class FilSocket implements Runnable{
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
     protected AstSocket socket;
     protected MonitorSync missatgeRebut;    
 
@@ -77,9 +87,13 @@ class FilSocket implements Runnable{
     public void run(){
         while(true){
             String txtEcho = socket.rebre();  //semantica bloquejant
-            System.out.println("Echo rebut : " + txtEcho);
-            System.out.println("");
-            missatgeRebut.avisa();
+            if (txtEcho.equals("north")) System.out.println(ANSI_BLUE + "Car heading north crossed bridge" + ANSI_RESET);
+            else if (txtEcho.equals("south")) System.out.println(ANSI_RED + "\t\t\t\t\t\tCar heading south crossed bridge" + ANSI_RESET);
+            else {
+                System.out.println("Echo rebut : " + txtEcho);
+                System.out.println("");
+                missatgeRebut.avisa();
+            }
         }
     }
 }
