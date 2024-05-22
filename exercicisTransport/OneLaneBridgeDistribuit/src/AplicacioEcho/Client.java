6a965b1c4633f62cecd6890330bf63010cc9e0a3
Elies García Alvira <eliesgarciaalvira@gmail.com>
2024-05-22 17:33:56 +0200

    Removed unnecesary legacy code

--- exercicisTransport/OneLaneBridgeDistribuit/src/AplicacioEcho/Client.java
+++ exercicisTransport/OneLaneBridgeDistribuit/src/AplicacioEcho/Client.java
@@ -4,43 +4,17 @@
  */
 package AplicacioEcho;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
-/**
- *
- * @author usuari.aula
- */
 public class Client {
     public static void main(String[] args){
-/*         new Thread(new ClientCar(Comms.IP_SERVIDOR, Comms.PORT_SERVIDOR, true)).start();
-        new Thread(new ClientCar(Comms.IP_SERVIDOR, Comms.PORT_SERVIDOR, true)).start();
-        new Thread(new ClientCar(Comms.IP_SERVIDOR, Comms.PORT_SERVIDOR, true)).start();
-        new Thread(new ClientCar(Comms.IP_SERVIDOR, Comms.PORT_SERVIDOR, true)).start();
-        new Thread(new ClientCar(Comms.IP_SERVIDOR, Comms.PORT_SERVIDOR, true)).start();
-        new Thread(new ClientCar(Comms.IP_SERVIDOR, Comms.PORT_SERVIDOR, true)).start();
-        new Thread(new ClientCar(Comms.IP_SERVIDOR, Comms.PORT_SERVIDOR, true)).start();
-        new Thread(new ClientCar(Comms.IP_SERVIDOR, Comms.PORT_SERVIDOR, false)).start();
-        new Thread(new ClientCar(Comms.IP_SERVIDOR, Comms.PORT_SERVIDOR, false)).start();
-        new Thread(new ClientCar(Comms.IP_SERVIDOR, Comms.PORT_SERVIDOR, false)).start();
-
-        for (int i = 0; i < 20; i++) {
-            double rand = Math.random();
-            if (rand > 0.3) {
-                new Thread(new ClientCar(Comms.IP_SERVIDOR, Comms.PORT_SERVIDOR, false)).start();
-            } else {
-                new Thread(new ClientCar(Comms.IP_SERVIDOR, Comms.PORT_SERVIDOR, true)).start();
-            }
-        }
- */
+        String sentitDecidit;
         PontRepresentant pont = new PontRepresentant(Comms.IP_SERVIDOR, Comms.PORT_SERVIDOR);
-        new Thread(new Cotxe(pont, Comms.NORT)).start();
+        double rand = Math.random();
+        if (rand > 0.5) {
+            sentitDecidit = Comms.NORT;
+        } else {
+            sentitDecidit = Comms.SUR;
+        }
+        new Thread(new Cotxe(pont, sentitDecidit)).start();
     }
     
     
@@ -83,134 +57,22 @@ class Cotxe implements Runnable {
     }
 
     public void run() {
-        pont.entrar(sentit);
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
+        for (int i = 0; i < 10; i++) {
+            pont.entrar(sentit);
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            pont.sortir();
+
+            double rand = Math.random();
+            if (rand > 0.5) {
+                sentit = Comms.NORT;
+            } else {
+                sentit = Comms.SUR;
+            }
         }
-        pont.sortir();
         pont.noTornar();
     }
-}
-
-class ClientCar implements Runnable{
-    protected AstSocket socket;
-    protected FilTeclat teclatTreaballador;
-    protected FilSocket socketTreballador;
-    protected MonitorSync missatgeRebut;
-    
-    public ClientCar(String ip, int port, boolean sentit){
-        socket = new AstSocket(ip, port);
-        missatgeRebut = new MonitorSync();
-        teclatTreaballador = new  FilTeclat(socket, missatgeRebut, sentit);
-        socketTreballador = new FilSocket(socket, missatgeRebut);
-
-    }
-    
-    public void run(){
-
-        new Thread(teclatTreaballador).start(); 
-        new Thread(socketTreballador).start();
-    }
-}
-
-class FilSocket implements Runnable{
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
-
-    protected AstSocket socket;
-    protected MonitorSync missatgeRebut;    
-
-    public FilSocket(AstSocket sc, MonitorSync mon){
-        socket = sc;
-        missatgeRebut = mon;
-    }    
-    
-    public void run(){
-        while(true){
-            String txtEcho = socket.rebre();  //semantica bloquejant
-            if (txtEcho.equals("north")) System.out.println(ANSI_BLUE + "Car heading north crossed bridge" + ANSI_RESET);
-            else if (txtEcho.equals("south")) System.out.println(ANSI_RED + "\t\t\t\t\t\tCar heading south crossed bridge" + ANSI_RESET);
-            else {
-                System.out.println("Echo rebut : " + txtEcho);
-                System.out.println("");
-                missatgeRebut.avisa();
-            }
-        }
-    }
-}
-
-class FilTeclat implements Runnable{
-    protected AstSocket socket;
-    protected BufferedReader entradaUsuari;
-    protected MonitorSync missatgeRebut;
-    protected String sentitCar;
-    
-    public FilTeclat(AstSocket sc, MonitorSync mon, boolean sentit){
-        socket = sc;
-        entradaUsuari = new BufferedReader (new InputStreamReader(System.in));
-        missatgeRebut = mon;
-        if (sentit) sentitCar = "north";
-        else if (!sentit) sentitCar = "south";
-    }
-
-    public FilTeclat(AstSocket sc, MonitorSync mon){
-        socket = sc;
-        entradaUsuari = new BufferedReader (new InputStreamReader(System.in));
-        missatgeRebut = mon;
-        sentitCar = "chat";
-    }
-
-    public void run(){
-        try {
-            if (sentitCar.equals("north")) socket.enviar("north");
-            else if (sentitCar.equals("south")) socket.enviar("south");
-            else {
-                while (true) {
-                    System.out.println("entra miss : ");
-                
-                    String txtUsuari = entradaUsuari.readLine(); //semantica bloquejant
-
-                    System.out.println("");
-                    socket.enviar(txtUsuari);
-
-                    if (txtUsuari.toLowerCase().equals("exit")) System.exit(0);
-                }
-            }
-            
-            missatgeRebut.espera();
-        } catch (IOException ex) {
-            Logger.getLogger(FilTeclat.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-}
-
-class MonitorSync{
-    protected Lock mon;
-    protected Condition echoRebut;
-    
-    public MonitorSync(){
-        mon = new ReentrantLock();
-        echoRebut = mon.newCondition();
-    }
-    
-    public void espera(){
-        mon.lock();
-        echoRebut.awaitUninterruptibly();
-        mon.unlock();
-    }
-    
-    public void avisa(){
-        mon.lock();
-        echoRebut.signal();
-        mon.unlock();
-    }    
 }
--- exercicisTransport/OneLaneBridgeDistribuit/src/AplicacioEcho/Servidor.java
+++ exercicisTransport/OneLaneBridgeDistribuit/src/AplicacioEcho/Servidor.java
@@ -5,34 +5,19 @@
 package AplicacioEcho;
 
 import java.io.IOException;
-import java.net.InetAddress;
 import java.net.ServerSocket;
-import java.net.UnknownHostException;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.net.ssl.SSLServerSocket;
 
 /**
  *
  * @author usuari.aula
  */
 public class Servidor {
-    static boolean sentitActual;
-    static int cotxesSentitContrariEsperant, cotxesSentitActualEsperant;
-    static int cotxesEnTransit;
-    static int numberOfWorkers;
-    static Lock mon = new ReentrantLock();
-    static Condition sentitContrariPotPassar = mon.newCondition(); 
-    static Condition sentitActualPotPassar = mon.newCondition();
-
     public static void main(String[] args) {
-/*         ServidorEcho echo = new ServidorEcho(Comms.PORT_SERVIDOR);
-        System.out.println("Servidor Eco Escoltant ... Port : " + Comms.PORT_SERVIDOR + "\n");
-        new Thread(echo).start();
- */        
         ServidorPont sPont = new ServidorPont(Comms.PORT_SERVIDOR);
         System.out.println("Servidor Pont Escoltant ... Port : " + Comms.PORT_SERVIDOR + "\n");
         new Thread(sPont).start();
@@ -71,6 +56,7 @@ class Treballador implements Runnable {
     protected AstSocket socket;
     protected Pont pont;
     protected boolean running;
+    protected String sentitActual;
 
     public Treballador(AstSocket s, Pont p) {
         socket = s;
@@ -81,18 +67,19 @@ class Treballador implements Runnable {
         while (running) {
             try {
                 String accio = socket.rebre();
-                System.out.println("Receivec accio: " + accio);
+                //System.out.println("Receivec accio: " + accio);
                 String sentit = socket.rebre();
-                System.out.println("Receivec sentit: " + sentit);
+                //System.out.println("Receivec sentit: " + sentit);
 
                 switch (accio) {
                     case Comms.ENTRAR:
+                        sentitActual = sentit;
                         pont.entrar(sentit);
-                        socket.enviar("COTXE HA ENTRAT AL PONT");
+                        socket.enviar("COTXE HA ENTRAT AL PONT PER " + sentitActual);
                         break;
                     case Comms.SORTIR:
                         pont.sortir();
-                        socket.enviar("COTXE HA SORTIT DEL PONT");
+                        socket.enviar("COTXE HA SORTIT DEL PONT PER " + sentitActual);
                         break;
                     case Comms.NEVER:
                         running = false;
@@ -102,145 +89,6 @@ class Treballador implements Runnable {
             } catch (NullPointerException ex) {
                 System.out.println(ex);
             }
-        }
-    }
-}
-
-class ServidorEcho implements Runnable{
-    protected ServerSocket ss;
-    
-    public ServidorEcho(int port){
-        try {
-            ss = new ServerSocket(port);
-        } catch (IOException ex) {
-            Logger.getLogger(ServidorEcho.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-    
-    public void run(){
-        while(true){
-            try {
-                PontReal pont = new PontReal();
-                while(true){
-                    AstSocket s = new AstSocket(ss.accept());
-                    System.out.println("Received new foreign connection");
-                    Servidor.numberOfWorkers++;
-                    new Thread(new Worker(s, Servidor.numberOfWorkers)).start();
-                }
-            } catch (IOException ex) {
-                Logger.getLogger(ServidorEcho.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
-    }
-}
-
-class Worker implements Runnable {
-    protected AstSocket socket;
-    protected boolean sentitCotxe, validRebut, running;
-    protected int workerId;
-    String rebut = new String();
-
-    public Worker(AstSocket s, int id) {
-        socket = s;
-        workerId = id;
-    }
-
-    public void run() {
-        System.out.println("Worker " + workerId + " started and ready to receive");
-        running = true;
-        while (running) {
-            validRebut = false;
-            try {
-                rebut = socket.rebre(); //semantica bloquejant
-                System.out.println("Worker " + workerId + " receives following message: " + rebut);
-
-                switch (rebut.toLowerCase()) {
-                    case "north":
-                        validRebut = true;
-                        sentitCotxe = true;
-                        break;
-                    
-                    case "south":
-                        validRebut = true;
-                        sentitCotxe = false;
-                        break;
-                    case "exit":
-                        running = false;               
-                    default:
-                        break;
-                }
-
-                if (validRebut) {
-                    // deja pasar si el sentido del coche es el mismo que el actual
-                    // el momento que viene un coche del sentido contrario se espera
-                    // y cuando se vacie el sentido actual cambia y continua la lógica
-
-                    // El mismo thread solo indica el sentido la primera vez que se llamas
-                    // porque las siguientes por fuerza es el contrario del anterior
-                    this.entrar(); // blockejant
-                    try {
-                        Thread.sleep(2000);
-                    } catch (InterruptedException e) {
-                        Thread.currentThread().interrupt();
-                    }
-                    this.sortir(rebut);
-                } else if (running) {
-                    socket.enviar(rebut);
-                }
-            } catch (NullPointerException ex) {
-                running = false;
-                //System.out.println(ex);
-            }
-        }
-        Servidor.mon.lock();
-        Servidor.numberOfWorkers--;
-        System.out.println("Worker " + workerId + " finished connection");
-        System.out.println(Servidor.numberOfWorkers + " workers left.");
-        Servidor.mon.unlock();
-    }
-
-    public void entrar() {
-        Servidor.mon.lock();
-        try {
-            if (Servidor.cotxesEnTransit == 0 && Servidor.cotxesSentitContrariEsperant == 0) {
-                Servidor.sentitActual = sentitCotxe;
-            }
-            while (Servidor.cotxesSentitContrariEsperant > 0) {
-                Servidor.cotxesSentitActualEsperant++;
-                Servidor.sentitActualPotPassar.awaitUninterruptibly();
-                Servidor.cotxesSentitActualEsperant--;
-            }
-
-            while (Servidor.sentitActual != sentitCotxe) {
-                Servidor.cotxesSentitContrariEsperant++;
-                Servidor.sentitContrariPotPassar.awaitUninterruptibly();
-                Servidor.cotxesSentitContrariEsperant--;
-                if (Servidor.cotxesSentitContrariEsperant == 0) {
-                    Servidor.sentitActualPotPassar.signalAll();
-                }
-            }
-
-            Servidor.cotxesEnTransit++;
-            System.out.println("ENTRA sentit " + sentitCotxe);
-        } catch (Exception ex) {
-            System.out.println(ex);
-        } finally {
-            Servidor.mon.unlock();
-        }
-    }
-
-    public void sortir(String rebut) {
-        Servidor.mon.lock();
-        try {
-            Servidor.cotxesEnTransit--;
-            if (Servidor.cotxesEnTransit == 0 && Servidor.cotxesSentitContrariEsperant > 0) {
-                Servidor.sentitActual = !Servidor.sentitActual;
-                Servidor.sentitContrariPotPassar.signalAll();
-            }
-            System.out.println("SURT sentit " + sentitCotxe);
-            socket.enviar(rebut);
-        } finally {
-            Servidor.mon.unlock();
         }
     }
 }
@@ -263,7 +111,7 @@ class PontReal implements Pont {
         try {
             boolean sentitMeu = false; // default
             if (sentitCotxe.equals(Comms.NORT)) sentitMeu = true;
-            else if (sentitCotxe.equals(Comms.NORT)) sentitMeu = true;
+            else if (sentitCotxe.equals(Comms.SUR)) sentitMeu = true;
             else {
                 System.out.println("FORMAT DE SENTIT INCORRECTE");
                 System.exit(1);
